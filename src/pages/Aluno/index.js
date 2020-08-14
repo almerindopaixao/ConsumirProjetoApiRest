1a35ac5ddc5f028b1c6270c696fc2c0683d58863
Almerindo Paixão <almerindopaixao@gmail.com>
2020-08-13 22:31:59 -0300

    get data of data base

--- src/pages/Aluno/index.js
+++ src/pages/Aluno/index.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { get } from 'lodash';
+import { isEmail, isInt, isFloat } from 'validator';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import { useHistory } from 'react-router-dom';
+
+import axios from '../../services/axios';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './styled';
+import Loading from '../../components/Loading';
 
 export default function Aluno({ match }) {
   const id = get(match, 'params.id', 0);
@@ -13,13 +19,79 @@ export default function Aluno({ match }) {
   const [idade, setIdade] = useState('');
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!id) return;
+
+    async function getData() {
+      try {
+        setIsLoading(true);
+
+        const { data } = await axios.get(`/alunos/${id}`);
+
+        const Foto = get(data, 'Fotos[0].url', '');
+
+        setNome(data.nome);
+        setSobrenome(data.sobrenome);
+        setEmail(data.email);
+        setIdade(data.idade);
+        setPeso(data.peso);
+        setAltura(data.altura);
+      } catch (err) {
+        const status = get(err, 'response.status', 0);
+
+        const errors = get(err, 'response.data.errors', 0);
+
+        if (status === 400) errors.map((error) => toast.error(error));
+        history.push('/');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getData();
+  }, [id, history]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const formErrors = [];
+
+    if (nome.length < 3 || nome.length > 255) {
+      formErrors.push('Nome precisa ter entre 3 e 255 caractéres');
+    }
+
+    if (sobrenome.length < 3 || sobrenome.length > 255) {
+      formErrors.push('Sobrenome precisa ter entre 3 e 255 caractéres');
+    }
+
+    if (!isEmail(email)) {
+      formErrors.push('E-mail Inválido');
+    }
+
+    if (!isInt(String(idade))) {
+      formErrors.push('Idade Inválida');
+    }
+
+    if (!isFloat(String(peso))) {
+      formErrors.push('Peso Inválido');
+    }
+
+    if (!isFloat(String(altura))) {
+      formErrors.push('Altura Inválida');
+    }
+
+    if (formErrors) {
+      formErrors.map((error) => toast.error(error));
+    }
   };
 
   return (
     <Container>
+      <Loading isLoading={isLoading} />
+
       <h1>{id ? 'Editar Aluno' : 'Novo Aluno'}</h1>
 
       <Form onSubmit={handleSubmit}>
